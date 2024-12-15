575edd55e5658f3dc0cf96fbfac1422536fd1fbc
swaroop <swaroopaee98@gmail.com>
2024-12-15 13:27:57 +0530

    Add project files.

---
+++ RESTAPI_PROJ/Decorators/JwtAuthorize.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace RESTAPI_PROJ.Decorators; // Replace with your project's namespace
+public class JwtAuthorizeFilter : IAuthorizationFilter
+{
+    private readonly JwtSecurityTokenHandlerWrapper _jwtSecurityTokenHandler;
+
+    public JwtAuthorizeFilter(JwtSecurityTokenHandlerWrapper jwtSecurityTokenHandler)
+    {
+        _jwtSecurityTokenHandler = jwtSecurityTokenHandler;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // Check if the [Authorize] attribute is explicitly applied to the action or controller.
+        var hasAuthorizeAttribute = context.ActionDescriptor.EndpointMetadata
+            .Any(em => em is AuthorizeAttribute);
+
+        if (hasAuthorizeAttribute)
+        {
+            var token = context.HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                try
+                {
+                    // Validate the token and extract claims
+                    var claimsPrincipal = _jwtSecurityTokenHandler.ValidateJwtToken(token);
+
+                    // Extract the user ID from the token
+                    var userId = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                    context.HttpContext.Items["UserId"] = userId;
+                }
+                catch (Exception)
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+            else
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+
+}
---
+++ RESTAPI_PROJ/Program.cs
@@ -0,0 +1,83 @@
+global using RESTAPI_PROJ.Helpers;
+global using RESTAPI_PROJ.Middlewares;
+
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using dotenv.net;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using RESTAPI_PROJ.Decorators;
+using Serilog;
+
+DotEnv.Load();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Adding AUthentication as a service
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+        ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET")!)),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+// Configure the default authorization policy
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+            .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+            .RequireAuthenticatedUser()
+            .Build();
+
+});
+builder.Services.AddScoped<JwtSecurityTokenHandlerWrapper>();
+// Configuration for [Authorize] attribute with custom filters
+builder.Services.AddControllersWithViews(options => {
+    options.Filters.Add(typeof(JwtAuthorizeFilter));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("Log/application.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddSerilog();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//app.UseMiddleware<JwtMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+Log.CloseAndFlush();
